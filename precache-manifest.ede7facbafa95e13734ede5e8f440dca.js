7157d270a26a6ddd296000ba172527767149df52
AlexndrG <alexndr1@mail.ru>
2021-07-02 15:06:02 +0300

    Updates

---
+++ precache-manifest.ede7facbafa95e13734ede5e8f440dca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a635c0f72622490f0b54de3fb11422b",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "87e22f80f11ad99bb06b",
+    "url": "/TodoList/static/css/main.786ba39e.chunk.css"
+  },
+  {
+    "revision": "3649be2dc8b8e3193387",
+    "url": "/TodoList/static/js/2.d99582fe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.d99582fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e22f80f11ad99bb06b",
+    "url": "/TodoList/static/js/main.bd3e6887.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
